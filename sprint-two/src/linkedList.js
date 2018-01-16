d32244b2ffd8f2d739a2911fbc76626d10e1e7e5
jorgelerma <jorge@jorgelerma.com>
2018-01-16 11:24:42 -0600

    Creates class

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+
+  //for in loop:
+  //look in list for node that has node.next === null
+  //set that node.next === our new node
+    var node = new Node(value);
+    if (list.head === null) {
+      list.head = node;
+      list.tail = node;
+    }
+    else {
+      list.tail.next = node;
+      list.tail = node;
+    }
+
   };
 
   list.removeHead = function() {
+    var temp = list.head;
+    list.head = list.head.next;
+    return temp.value;
   };
 
   list.contains = function(target) {
+    var currentHead = this.head;
+    while(currentHead) {
+      if(target === currentHead.value) {
+        return true;
+      } else {
+        currentHead = currentHead.next;
+      }
+    }
+    return false;
   };
 
   return list;
@@ -23,6 +49,7 @@ var Node = function(value) {
 
   return node;
 };
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
