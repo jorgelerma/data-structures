451040abadcdc2dab530d3cd0f4792415e657845
jorgelerma <jorge@jorgelerma.com>
2018-01-16 11:21:59 -0600

    Creates class

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,22 +1,42 @@
+//************
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //create a child
+  var newChild = new Tree(value);
+  //push into children array
+  this.children.push(newChild);
 };
 
 treeMethods.contains = function(target) {
+  if(this.value === target){
+    return true;
+  }
+
+  //loop through the children array
+  for(var i = 0; i < this.children.length; i++){
+    //compare if this value is equal to target
+    var child = this.children[i];
+    if(child.contains(target)){
+      //return true
+      return true;
+    }
+    
+  }
+
+  return false;
+
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
