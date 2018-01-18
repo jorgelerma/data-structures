b8e84eea1f39b8f7886151a46440d488549dc648
jorgelerma <jorge@jorgelerma.com>
2018-01-18 14:20:37 -0600

    Creates class

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,42 @@
 var Queue = function() {
-  var someInstance = {};
+  var count = 0;
 
   // Use an object with numeric keys to store values
+  //var storage = {name:"Jason"};
   var storage = {};
-
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+
+    //var len = Object.keys(storage).length;
+    //console.log(len);
+    //console.log(typeof len);
+
+
+    storage[count] = value;
+    count++;
+
+    //var indexer =
+    //var len = storage[Object.keys(storage)[Object.keys(storage).length-1]];
   };
+
+
 
   someInstance.dequeue = function() {
-  };
+
+    var len = Object.keys(storage).length;
+    //console.log(len);console.log("Printing it out: ");
+    //console.log(typeof len);
+    var value = storage[Object.keys(storage)[0]];
+    delete storage[Object.keys(storage)[0]];
+    //console.log("dequeue value: " + value);
+    //return console.log("dequeue value: " + value);
+
+
+    return value;
+    };
 
   someInstance.size = function() {
-  };
 
-  return someInstance;
+    return Object.keys(storage).length;
+  };
 };
